b761a7ba42d19f7a4e548ddf14728c7114dbab6b
unknown <joaco.andreswylie@gmail.com>
2022-04-05 19:02:08 -0400

    agregamosCalculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+
+ 
+/**
+ *
+ * @author waki
+ */
+public class Calculadora extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+        
+       
+    }
+      public static void main(String[] args) {
+        
+        launch(args);}     
+      
+}
