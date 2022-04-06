4f985072f661d5b85d9a0327902b0cebdcafcb28
Joaquín Wylie <102191624+wakictm@users.noreply.github.com>
2022-04-06 19:23:50 -0400

    el boton dividir solo funcionara con el 1 por ahora y se agrego un ArrayList para hacer funcionar esto

--- Calculadora/src/calculadora/FXMLDocumentController.java
+++ Calculadora/src/calculadora/FXMLDocumentController.java
@@ -6,6 +6,7 @@
 package calculadora;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -20,7 +21,7 @@ import javafx.scene.shape.ArcType;
  * @author krox2
  */
 public class FXMLDocumentController implements Initializable {
-    
+        
     @FXML
     private Button bttn_mas, bttn_menos, bttn_0, bttn_1, bttn_2, bttn_3, bttn_4, bttn_5, bttn_6, bttn_7, bttn_x, bttn_parentesisDerecho,bttn_parentesisIzquierdo ;
     int mx = 0;
@@ -37,18 +38,25 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Button bbtn_dividir;
     
+    int posicion =0;
+        
+    ArrayList<String> operacion = new ArrayList<String>();
+    
+
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+    
     }    
     
     
     @FXML
     public void accion_mas(ActionEvent event) {//listo
       gc = texthere.getGraphicsContext2D();
-      gc.strokeLine(mx + 22.5,37.5,mx +22.5,62.5);        //Altura maxima = 25, Altura Minima, 75
-      gc.strokeLine(mx +10,50,mx +35,50);        //Ancho maximo = 30, parte desde x = 10
+      gc.strokeLine(mx + 7.5,37.5,mx +7.5,62.5);        //Altura maxima = 25, Altura Minima, 75
+      gc.strokeLine(mx -3,50,mx +20,50);        //Ancho maximo = 30, parte desde x = 10
       mx = mx + 35;
+      
     }
     @FXML
     public void accion_menos(ActionEvent event) {//listo
@@ -69,11 +77,35 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     public void accion1(ActionEvent event) {//listo
-      gc = texthere.getGraphicsContext2D();
-      gc.strokeLine(mx + 20,20,mx + 20,50);
-      gc.strokeLine(mx + 20,20,mx + 15,30);
-      mx = mx + 20;
-    }
+      
+      if(operacion.isEmpty()){  
+           gc = texthere.getGraphicsContext2D();
+           gc.strokeLine(mx + 20,20,mx + 20,50);
+           gc.strokeLine(mx + 20,20,mx + 15,30);
+           mx = mx + 20;
+           operacion.add("1");
+           posicion = posicion + 1;
+           }
+      
+      
+      else if(operacion.get(operacion.size()-1) != "/"){  
+           gc = texthere.getGraphicsContext2D();
+           gc.strokeLine(mx + 20,20,mx + 20,50);
+           gc.strokeLine(mx + 20,20,mx + 15,30);
+           mx = mx + 20;
+           operacion.add("1");
+           posicion = posicion + 1;
+           }
+      
+      else if(operacion.get(operacion.size()-1) == "/"){  
+           gc = texthere.getGraphicsContext2D();
+           gc.strokeLine(mx + -20,70,mx + -20,100);
+           gc.strokeLine(mx + -20,70,mx + -25,80);
+           mx = mx + 20;
+           operacion.add("1");
+           posicion = posicion + 1;
+      
+      }}
     
     @FXML
     public void accion2(ActionEvent event) {
@@ -182,16 +214,17 @@ public class FXMLDocumentController implements Initializable {
         
         gc = texthere.getGraphicsContext2D();
         gc.clearRect(0, 0, 1000, 1000);
+        mx=0;
     }
     //nuevo
      @FXML
     private void accion_dividir(ActionEvent event) {
          gc = texthere.getGraphicsContext2D();
-         gc.strokeLine( 0,60, 40,60);
-         
-         
-         gc.strokeLine( 20,70, 20,100);
-         gc.strokeLine( 20,70, 15,80);
+         gc.strokeLine(mx +-20,60,mx + 20,60);
+      
+         operacion.add("/");
+         mx = mx + 20;
+         posicion = posicion + 1;
     }
     
     @FXML
