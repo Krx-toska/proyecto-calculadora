f398839795903d32259bbb1d307d818e9bb1dae1
ArturoAlvarezz <60265365+ArturoAlvarezz@users.noreply.github.com>
2022-05-04 18:08:29 -0400

    programa funcionando

--- Calculadora/src/calculadora/Calculadora.java
+++ Calculadora/src/calculadora/Calculadora.java
@@ -1,5 +1,38 @@
-package src.calculadora;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
 
-public class Calculadora {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+/**
+ *
+ * @author krox2
+ */
+public class Calculadora extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("calculadora");
+        stage.show();
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
 }
