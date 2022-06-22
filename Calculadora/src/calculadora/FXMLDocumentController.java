aefb845d0675089d90113ab84224c03f797a0b3b
ArturoAlvarezz <60265365+ArturoAlvarezz@users.noreply.github.com>
2022-06-22 15:48:05 -0400

    bugs textToDraw reparados

--- Calculadora/src/calculadora/FXMLDocumentController.java
+++ Calculadora/src/calculadora/FXMLDocumentController.java
@@ -274,11 +274,13 @@ public class FXMLDocumentController implements Initializable {
   void textToDraw(ActionEvent event) {
     String newOp = inputText.getText();
     operacion.clear();
+    myText.setText("");
     for (int i = 0; i < newOp.length(); i++) {
       operacion.add(String.valueOf(newOp.charAt(i)));
     }
     gc.clearRect(0, 0, 1000, 1000);
     hola.redibujar(operacion, gc,myColorOperators,myColorNumbers,sizeFactor);
+    updateText();
     cordenadas.limpiar();
   }
 
